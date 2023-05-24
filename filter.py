bc0eeb7471ca4fdf3d3dcf0d9f27e7fa4a52d1c2
Hsiao-Chien-Ti <0410adi@gmail.com>
2023-05-24 23:03:55 +0800

    remove filter

--- filter.py
+++ filter.py
@@ -1,5 +1,5 @@
 from scipy.signal import butter,iirnotch, filtfilt
-from numpy import *
+import numpy as np
 import pyqtgraph as pg
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -7,13 +7,12 @@ from PyQt5.QtWidgets import *
 import interface
 from PyQt5.QtWidgets import QApplication
 from datetime import datetime
-import os.path
 import sys
 def filter(data, notchcut,q,lowcut, highcut, fs, order=4):
     b,a=iirnotch(notchcut,q,fs)
     y = filtfilt(b, a, data)
     # print(y)
-    avg = mean(y)
+    avg = np.mean(y)
     y = y - avg
     b, a = butter(order, [lowcut, highcut],fs=fs, btype='band')
     y = filtfilt(b, a, y)
@@ -44,11 +43,13 @@ def update():
     for i in range(4):
         X[i][:-1]=X[i][1:]   # shift data in the temporal mean 1 sample left    
         value = float(float(interf.read())*5/255)               # read line (single value) from the serial port
+        # value = np.random.uniform(low=0, high=5)
         X[i][-1] = value                 # vector containing the instantaneous values  
-        filtRes=filter(X[i],60,0.5,10.2,410,1000)
-        curve[i].setData(filtRes)        
+        # filtRes=filter(X[i],60,0.5,10.2,410,1000)
+        # curve[i].setData(filtRes)
+        curve[i].setData(X[i])        
     QApplication.processEvents()    # you MUST process the plot now
-gestures = ['gesture0','gesture1','gesture2']
+gestures = ['gesture0','gesture1','gesture2','gesture3','gesture4','gesture5']
 gesture = int(input("Gesture: ")) # use integer for gesture type
 interf = interface.interface()
 app = QApplication(sys.argv)
@@ -71,7 +72,7 @@ p3.setYRange(0, 5, padding=0)
 p4.setYRange(0, 5, padding=0)
 curve=[p1.plot(),p2.plot(),p3.plot(),p4.plot()]
 windowWidth = 1000                      # width of the window displaying the curve
-X =[linspace(0,0,windowWidth),linspace(0,0,windowWidth),linspace(0,0,windowWidth),linspace(0,0,windowWidth)]
+X =[np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth)]
 # X=[np.random.uniform(low=0, high=5, size=windowWidth),np.random.uniform(low=0, high=5, size=windowWidth),np.random.uniform(low=0, high=5, size=windowWidth),np.random.uniform(low=0, high=5, size=windowWidth)]
 ptr = -windowWidth                      # set first x position
 interf.write('s')
