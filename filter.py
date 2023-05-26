59650ede05c83e68e80f9296bb0850cdd0a81571
Hsiao-Chien-Ti <0410adi@gmail.com>
2023-05-26 22:19:32 +0800

    fix pyqt thread

--- filter.py
+++ filter.py
@@ -10,31 +10,59 @@ from datetime import datetime
 import sys
 gestures = ['gesture0','gesture1','gesture2','gesture3','gesture4','gesture5','gesture6']
 gesture = int(input("Gesture: ")) # use integer for gesture type
-interf = interface.interface("COM42")
-
 windowWidth = 1000                      # width of the window displaying the curve
-X =[np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth)]
-
-class Worker(QThread):
-    global X
+class Worker(QThread):    
     data1 = pyqtSignal(object)
     data2 = pyqtSignal(object)
     data3 = pyqtSignal(object)
     data4 = pyqtSignal(object)
+    leave = pyqtSignal(object)
     def __init__(self):
         super().__init__()
-        interf.write('s')
+        self.interf = interface.interface("COM17")
+        self.interf.write('s')
+        self.X =[np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth),np.linspace(0,0,windowWidth)]
+        self.running=1
     def run(self):
-        while(1):
+        while(self.running):
             for i in range(4):
-                X[i][:-1]=X[i][1:]   # shift data in the temporal mean 1 sample left    
-                value = float(float(interf.read())*5/255)               # read line (singl value) from the serial port
-                X[i][-1] = value
-            self.data1.emit(X[0])
-            self.data2.emit(X[1])
-            self.data3.emit(X[2])
-            self.data4.emit(X[3])
+                self.X[i][:-1]=self.X[i][1:]   # shift data in the temporal mean 1 sample left    
+                value = float(float(self.interf.read())*5/255)               # read line (singl value) from the serial port
+                self.X[i][-1] = value
+            self.data1.emit(self.X[0])
+            self.data2.emit(self.X[1])
+            self.data3.emit(self.X[2])
+            self.data4.emit(self.X[3])
+        self.interf.write('e')
+        self.interf.end_process()
+        self.leave.emit(1)
+        # sys.exit(0)
+        # self.exit()
+    def make_connection(self, data_object):
+        data_object.save.connect(self.save)
+        data_object.end.connect(self.end)
+    @pyqtSlot(object)
+    def save(self, save):
+        if save == 1:
+            with open('training_data/'+gestures[gesture]+'_'+datetime.strftime(datetime.now(),'%Y_%m_%d_%H_%M_%S')+'.txt','w') as f:
+                f.write('time\tchannel1\tchannel2\tchannel3\tchannel4\tclass\n')
+                for i in range(windowWidth):
+                    f.write(str(i))
+                    f.write('\t')
+                    for j in range(4):
+                        f.write(str(round(self.X[j][i], 3)))
+                        f.write('\t')
+                    f.write(str(gesture))
+                    f.write('\n')
+    @pyqtSlot(object)
+    def end(self, end):
+        if end == 1:
+            self.running=0
+            
+    
 class Graph(QWidget):
+    save=pyqtSignal(object)
+    end=pyqtSignal(object)
     def __init__(self):
         super().__init__()
         self.layout = QVBoxLayout(self)
@@ -55,10 +83,10 @@ class Graph(QWidget):
         self.layout.addWidget(self.graph3)
         self.layout.addWidget(self.graph4)
         self.saveBtn = QPushButton('save')
-        self.saveBtn.clicked.connect(saveData)
+        self.saveBtn.clicked.connect(self.saveData)
         self.layout.addWidget(self.saveBtn)
         endBtn = QPushButton('end')
-        endBtn.clicked.connect(end)
+        endBtn.clicked.connect(self.endProgram)
         self.layout.addWidget(endBtn)
         self.show()
 
@@ -67,7 +95,11 @@ class Graph(QWidget):
         data_object.data2.connect(self.grab_data2)
         data_object.data3.connect(self.grab_data3)
         data_object.data4.connect(self.grab_data4)
-
+        data_object.leave.connect(self.terminate)
+    def saveData(self):
+        self.save.emit(1)
+    def endProgram(self):
+        self.end.emit(1)
     @pyqtSlot(object)
     def grab_data1(self, data):
         self.plot1.setData(data)
@@ -80,26 +112,14 @@ class Graph(QWidget):
     @pyqtSlot(object)
     def grab_data4(self, data):
         self.plot4.setData(data)
-def saveData():
-    global X
-    with open('training_data/'+gestures[gesture]+'_'+datetime.strftime(datetime.now(),'%Y_%m_%d_%H_%M_%S')+'.txt','w') as f:
-        f.write('time\tchannel1\tchannel2\tchannel3\tchannel4\tclass\n')
-        for i in range(windowWidth):
-            f.write(str(i))
-            f.write('\t')
-            for j in range(4):
-                f.write(str(round(X[j][i], 3)))
-                f.write('\t')
-            f.write(str(gesture))
-            f.write('\n')
-def end():
-    interf.write('e')
-    worker.terminate()
-    interf.end_process()
-    sys.exit(0)
+    @pyqtSlot(object)
+    def terminate(self, data):
+        if data==1:
+            sys.exit(0)
 app = QApplication(sys.argv)
 widget = Graph()
 worker = Worker()
 widget.make_connection(worker)
+worker.make_connection(widget)
 worker.start()
 sys.exit(app.exec_())
